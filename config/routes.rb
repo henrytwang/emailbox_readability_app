323f8f6693d6c265c8371e9cbe5d1e1c6f7f9c53
Henry Wang <henry.t.wang@gmail.com>
2013-08-19 19:30:39 -0700

    update manifest and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ EmailboxApp::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
-  match '/' => 'home#index'
-  match '/home' => 'home#home'
+  match '/' => 'home#home'
+  match '/incoming_action' => 'home#index'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
